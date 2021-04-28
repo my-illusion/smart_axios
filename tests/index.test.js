4ff5d3a3c5dd555ed791cd539a7e392e42b883eb
宋冰冰 <wb.songbingbing@mesg.corp.netease.com>
2021-04-28 18:23:57 +0800

    init

---
+++ tests/index.test.js
@@ -0,0 +1,17 @@
+const http = require("http")
+const {default: axios} = require('../src/index')
+
+let server
+
+server = http.createServer(function (req, res) {
+  setTimeout(function () {
+    res.end('1');
+  }, 100);
+}).listen(4444, 'localhost', function () {
+  var success = false
+  axios.get('http://localhost:4444/').then(function (res) {
+    success = true;
+    console.log(111, res.data)
+    server.close()
+  })
+});
--- tests/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { add } from "../src";
-
-describe("test index.ts", () => {
-  test("add", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
